1be43c00d82c7e49584f094460245ff34d938d56
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-11-07 13:53:59 +0300

    Signup user

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -1,7 +1,56 @@
+import { matchedData, validationResult } from "express-validator";
+import { validateLogin } from "../lib/validation.js";
+import asyncHandler from "express-async-handler";
+import bcrypt from "bcryptjs";
+import prisma from "../lib/client.js";
+
 const signup = {
     get(req, res) {
         res.render("signup");
     },
+    post: [
+        validateLogin(),
+        asyncHandler(async (req, res, next) => {
+            const error = validationResult(req);
+
+            if (!error.isEmpty()) {
+                const errors = error.array({ onlyFirstError: true });
+
+                res.status(400).render("signup", {
+                    username: req.body.username,
+                    usernameError: errors.find(
+                        (value) => value.path === "username",
+                    )?.msg,
+                    passwordError: errors.find(
+                        (value) => value.path === "password",
+                    )?.msg,
+                    confirmPasswordError: errors.find(
+                        (value) => value.path === "confirmPassword",
+                    )?.msg,
+                });
+
+                return;
+            }
+
+            const { username, password } = matchedData(req);
+
+            const user = await prisma.user.create({
+                data: {
+                    username,
+                    passwordHash: await bcrypt.hash(password, 10),
+                },
+            });
+
+            req.login(user, (error) => {
+                if (error) {
+                    next(error);
+                    return;
+                }
+
+                res.redirect("/");
+            })
+        }),
+    ],
 };
 
 export { signup };
