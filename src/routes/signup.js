b9a09ba4263b2b06dc6cd096290981d3d3b964ae
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-11-07 13:54:44 +0300

    Add post handler

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -3,6 +3,6 @@ import { signup } from "../controllers/signup.js";
 
 const router = Router();
 
-router.route("/").get(signup.get);
+router.route("/").get(signup.get).post(signup.post);
 
 export default router;
