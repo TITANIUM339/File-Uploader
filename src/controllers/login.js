807c8bb6b1a2d6426094655c375c98bfc4d5d90a
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-11-11 20:38:59 +0300

    Login user

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,7 +1,59 @@
+import { validateLogin } from "../lib/validation.js";
+import { validationResult } from "express-validator";
+import passport from "passport";
+
 const login = {
     get(req, res) {
         res.render("login");
     },
+    post: [
+        validateLogin(),
+        (req, res, next) => {
+            const error = validationResult(req);
+
+            if (!error.isEmpty()) {
+                const errors = error.array({ onlyFirstError: true });
+
+                res.status(400).render("login", {
+                    username: req.body.username,
+                    usernameError: errors.find(
+                        (value) => value.path === "username",
+                    )?.msg,
+                    passwordError: errors.find(
+                        (value) => value.path === "password",
+                    )?.msg,
+                });
+
+                return;
+            }
+
+            passport.authenticate("local", (err, user, info) => {
+                if (err) {
+                    next(err);
+                    return;
+                }
+
+                if (!user) {
+                    res.status(400).render("login", {
+                        username: req.body.username,
+                        usernameError: info.message,
+                        passwordError: info.message,
+                    });
+
+                    return;
+                }
+
+                req.login(user, (err) => {
+                    if (err) {
+                        next(err);
+                        return;
+                    }
+
+                    res.redirect("/");
+                });
+            })(req);
+        },
+    ],
 };
 
 export { login };
