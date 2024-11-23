117d9bbfa6ccacabcbb655c8cf2998b43df9af81
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-11-23 20:51:56 +0300

    Create popup

---
+++ public/scripts/sidebar.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+document.querySelectorAll(".sidebar-button").forEach((button, index) => {
+    const dialog = document.querySelectorAll("dialog")[index];
+
+    button.addEventListener("click", () => dialog.showModal());
+
+    dialog
+        .querySelector(".secondary-button")
+        .addEventListener("click", () => dialog.close());
+});
