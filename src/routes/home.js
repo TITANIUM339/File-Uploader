81ac83eec383efaa4b9711a614b8719818886844
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-12-03 16:24:37 +0300

    Add middleware that ensures path exists

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,7 +1,34 @@
 import { Router } from "express";
 import { home } from "../controllers/home.js";
+import asyncHandler from "express-async-handler";
+import prisma from "../lib/client.js";
 
 const router = Router();
+
+router.use(
+    asyncHandler(async (req, res, next) => {
+        if (!/^(?:\/[\w\-~.]+)*\/?$/.test(req.path)) {
+            next("router");
+
+            return;
+        }
+
+        const path = req.path.split("/").filter((value) => value !== "");
+
+        const [result] =
+            await prisma.$queryRaw`SELECT jsonb_path_exists(folder, ${path.length ? `$."${path.join('"."')}"` : "$"}::jsonpath) AS exists FROM "Home" WHERE id = ${req.user.homeId};`;
+
+        const { exists } = result;
+
+        if (!exists) {
+            next("router");
+
+            return;
+        }
+
+        next();
+    }),
+);
 
 router.route(/^(?:\/[^/]+)*\/?$/).get(home.get);
 
