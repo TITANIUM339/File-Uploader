48b730c6f7d85f5455a41b5bde420d600256a865
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-12-09 17:03:20 +0300

    Remove authentication check

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,36 +1,32 @@
 import asyncHandler from "express-async-handler";
-import isAuthenticated from "../lib/isAuthenticated.js";
 import prisma from "../lib/client.js";
 import { arrayToJsonpath, pathToArray } from "../lib/pathUtilities.js";
 
 const home = {
-    get: [
-        isAuthenticated,
-        asyncHandler(async (req, res) => {
-            const path = pathToArray(req.path);
+    get: asyncHandler(async (req, res) => {
+        const path = pathToArray(req.path);
 
-            const [result] =
-                await prisma.$queryRaw`SELECT jsonb_path_query(folder, ${arrayToJsonpath(path)}::jsonpath) AS items FROM "Home" WHERE id = ${req.user.homeId}`;
+        const [result] =
+            await prisma.$queryRaw`SELECT jsonb_path_query(folder, ${arrayToJsonpath(path)}::jsonpath) AS items FROM "Home" WHERE id = ${req.user.homeId}`;
 
-            const { items } = result;
+        const { items } = result;
 
-            const files = Object.keys(items)
-                .filter((value) => !value.includes("$"))
-                .map((value) => ({
-                    name: value,
-                    path: `/home${req.path === "/" ? "" : req.path}/${value}`,
-                    size: items[value].$size,
-                    date: items[value].$date,
-                    iconClassname: items[value].$size ? "" : "bi bi-folder",
-                }));
+        const files = Object.keys(items)
+            .filter((value) => !value.includes("$"))
+            .map((value) => ({
+                name: value,
+                path: `/home${req.path === "/" ? "" : req.path}/${value}`,
+                size: items[value].$size,
+                date: items[value].$date,
+                iconClassname: items[value].$size ? "" : "bi bi-folder",
+            }));
 
-            res.render("home", {
-                path: req.path,
-                breadcrumb: ["home", ...path],
-                files,
-            });
-        }),
-    ],
+        res.render("home", {
+            path: req.path,
+            breadcrumb: ["home", ...path],
+            files,
+        });
+    }),
 };
 
 export { home };
