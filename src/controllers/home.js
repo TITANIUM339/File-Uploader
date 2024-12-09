3aa6664a5e32f02ba912c38aa99f69302e24137a
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-12-09 16:07:32 +0300

    Display user files

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,17 +1,37 @@
+import asyncHandler from "express-async-handler";
 import isAuthenticated from "../lib/isAuthenticated.js";
+import prisma from "../lib/client.js";
 
 const home = {
     get: [
         isAuthenticated,
-        (req, res) => {
+        asyncHandler(async (req, res) => {
+            const path = req.path.split("/").filter((value) => value !== "");
+
+            const [result] =
+                await prisma.$queryRaw`SELECT jsonb_path_query(folder, ${path.length ? `$."${path.join('"."')}"` : "$"}::jsonpath) AS items FROM "Home" WHERE id = ${req.user.homeId}`;
+
+            const { items } = result;
+
+            const files = Object.keys(items)
+                .filter((value) => !value.includes("$"))
+                .map((value) => ({
+                    name: value,
+                    path: `/home${req.path === "/" ? "" : req.path}/${value}`,
+                    size: items[value].$size,
+                    date: items[value].$date,
+                    iconClassname: items[value].$size ? "" : "bi bi-folder",
+                }));
+
             res.render("home", {
                 path: req.path,
                 breadcrumb: [
                     "home",
                     ...req.path.split("/").filter((value) => value !== ""),
                 ],
+                files,
             });
-        },
+        }),
     ],
 };
 
