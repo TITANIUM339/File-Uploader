ab4319a53c41eb10bb8ed07757703262c56693b1
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-12-16 14:52:37 +0300

    Create constants file

---
+++ src/lib/constants.js
@@ -0,0 +1,5 @@
+const NAME_PATTERN = /^(?:\.*[\w\-~]+)+$/;
+const USERNAME_PATTERN = /^\w+$/;
+const PATH_PATTERN = /^(?:\/[\w\-~.]+)*\/?$/;
+
+export { NAME_PATTERN, USERNAME_PATTERN, PATH_PATTERN };
