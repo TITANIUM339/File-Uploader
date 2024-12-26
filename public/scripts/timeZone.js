8b97605ba4069488b66d9b803fe009b4bca44540
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-12-26 15:28:32 +0300

    Append date value instead of reassigning

--- public/scripts/timeZone.js
+++ public/scripts/timeZone.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-undef */
 document.querySelectorAll(".date").forEach((element) => {
-    element.innerText = new Date(element.dataset.date).toLocaleString("en-US", {
-        year: "numeric",
-        month: "numeric",
-        day: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        hour12: true,
-    });
+    element.innerText += new Date(element.dataset.date).toLocaleString(
+        "en-US",
+        {
+            year: "numeric",
+            month: "numeric",
+            day: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+            hour12: true,
+        },
+    );
 });
