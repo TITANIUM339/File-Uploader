59ddeb37b57ebf2b40d083235c24d65e14fd228c
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-12-28 11:53:55 +0300

    Create queries

---
+++ src/lib/queries.js
@@ -0,0 +1,69 @@
+import { Prisma } from "@prisma/client";
+import { UTCDate } from "@date-fns/utc";
+import { arrayToJsonpath } from "./pathUtilities.js";
+
+const addFile = `UPDATE "Home" SET folder = jsonb_insert(folder, $1::text[], $2::jsonb) WHERE id = $3`;
+const fileDoesNotAlreadyExist = `AND NOT jsonb_path_exists(folder, $4::jsonpath)`;
+const pathIsFolder = `AND COALESCE(folder #>> $5::text[], 'folder') = 'folder'`;
+
+function pathExists(jsonpath, id) {
+    return Prisma.sql`SELECT jsonb_path_exists(folder, ${jsonpath}::jsonpath) AS exists FROM "Home" WHERE id = ${id}`;
+}
+
+function addNewFile(arrayFilePath, file, id) {
+    const newFile = {
+        $type: "file",
+        $mimeType: file.mimetype,
+        $size: file.size,
+        $location: file.path,
+        $extension:
+            /^.+\.([A-Za-z0-9]+)$/.exec(file.originalname)?.[1].toLowerCase() ||
+            null,
+        $date: new UTCDate(),
+    };
+
+    const query = Prisma.raw(
+        `${addFile} ${fileDoesNotAlreadyExist} ${pathIsFolder}`,
+    );
+
+    query.values = [
+        arrayFilePath,
+        newFile,
+        id,
+        arrayToJsonpath(arrayFilePath),
+        [...arrayFilePath.slice(0, arrayFilePath.length - 1), "$type"],
+    ];
+
+    return query;
+}
+
+function addNewFolder(arrayFolderPath, id) {
+    const newFolder = {
+        $type: "folder",
+        $date: new UTCDate(),
+    };
+
+    const query = Prisma.raw(
+        `${addFile} ${fileDoesNotAlreadyExist} ${pathIsFolder}`,
+    );
+
+    query.values = [
+        arrayFolderPath,
+        newFolder,
+        id,
+        arrayToJsonpath(arrayFolderPath),
+        [...arrayFolderPath.slice(0, arrayFolderPath.length - 1), "$type"],
+    ];
+
+    return query;
+}
+
+function getFiles(jsonpath, id) {
+    return Prisma.sql`SELECT jsonb_path_query(folder, ${jsonpath}::jsonpath) AS items FROM "Home" WHERE id = ${id}`;
+}
+
+function isFolder(arrayPath, id) {
+    return Prisma.sql`SELECT COALESCE(folder #>> ${[...arrayPath, "$type"]}::text[], 'folder') = 'folder' AS folder FROM "Home" WHERE id = ${id}`;
+}
+
+export { pathExists, addNewFile, addNewFolder, getFiles, isFolder };
