97b3e449c945bc3ac2e091caccb10bc60ff197c1
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-01-01 14:46:49 +0300

    Add delete-folder post handler

--- src/routes/folder.js
+++ src/routes/folder.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { newFolder } from "../controllers/folder.js";
+import { deleteFolder, newFolder } from "../controllers/folder.js";
 
 const router = Router();
 
 router.post("/new-folder", newFolder.post);
+router.post("/delete-folder", deleteFolder.post);
 
 export default router;
