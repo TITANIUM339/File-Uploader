77d46bdfa6d0a875e5b660605c55bd780cd1c9b6
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-01-02 17:55:59 +0300

    Add rename post handler

--- src/routes/file.js
+++ src/routes/file.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { deleteFile, download, newFile } from "../controllers/file.js";
+import { deleteFile, download, newFile, rename } from "../controllers/file.js";
 
 const router = Router();
 
 router.post("/new-file", newFile.post);
 router.post("/download", download.post);
 router.post("/delete-file", deleteFile.post);
+router.post("/rename", rename.post);
 
 export default router;
