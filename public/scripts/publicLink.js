86f8e55439f3ca051cc453091101cb64e607dcd1
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-01-06 17:45:00 +0300

    Display public link and copy to clip board

---
+++ public/scripts/publicLink.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+(() => {
+    const link = document.querySelector(".public-link-content");
+
+    link.innerText = `${window.location.origin}/share/${link.dataset.id}`
+
+    document
+        .querySelector(".public-link > button")
+        .addEventListener("click", () => navigator.clipboard.writeText(link.innerText));
+})();
