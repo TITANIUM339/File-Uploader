b452425a92c98ac03e3de6159137040d723bd0ed
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2025-01-08 15:44:29 +0300

    Create share router

---
+++ src/routes/share.js
@@ -0,0 +1,83 @@
+import { Router } from "express";
+import { folder } from "../controllers/share.js";
+import { PATH_PATTERN } from "../lib/constants.js";
+import asyncHandler from "express-async-handler";
+import prisma from "../lib/client.js";
+import { UTCDate } from "@date-fns/utc";
+import { pathToArray } from "../lib/pathUtilities.js";
+import { isFolder } from "../lib/queries.js";
+import { pathExists } from "../lib/queries.js";
+import { arrayToJsonpath } from "../lib/pathUtilities.js";
+
+const router = Router();
+
+router.use(
+    "/:id",
+    asyncHandler(async (req, res, next) => {
+        const share = await prisma.share.findUnique({
+            where: { id: req.params.id, expiresAt: { gt: new UTCDate() } },
+            include: { user: true },
+        });
+
+        if (!share) {
+            next("router");
+
+            return;
+        }
+
+        res.locals.shareId = share.id;
+        res.locals.sharePath = share.path;
+        res.locals.homeId = share.user.homeId;
+
+        next();
+    }),
+);
+
+router.get(
+    PATH_PATTERN,
+    asyncHandler(async (req, res, next) => {
+        const path = [
+            ...pathToArray(res.locals.sharePath),
+            ...pathToArray(req.path).slice(1),
+        ];
+
+        const [result] = await prisma.$queryRaw(
+            pathExists(arrayToJsonpath(path), res.locals.homeId),
+        );
+
+        const { exists } = result;
+
+        if (!exists) {
+            next("router");
+
+            return;
+        }
+
+        next();
+    }),
+    asyncHandler(async (req, res, next) => {
+        const path = [
+            ...pathToArray(res.locals.sharePath),
+            ...pathToArray(req.path).slice(1),
+        ];
+
+        console.log(path);
+
+        const [result] = await prisma.$queryRaw(
+            isFolder(path, res.locals.homeId),
+        );
+
+        const { folder } = result;
+
+        if (!folder) {
+            next("route");
+
+            return;
+        }
+
+        next();
+    }),
+    folder.get,
+);
+
+export default router;
